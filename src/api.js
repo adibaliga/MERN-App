7ef513265f490ffbfff4292f6cb40c104492fa93
shobhana.m.e <shobhana.m.e@thalesdigital.io>
2021-07-01 15:26:47 +0530

    no backend

--- src/api.js
+++ src/api.js
@@ -7,11 +7,14 @@
     
 //   });
 
-export const getTodos = () => fetch("http://localhost:4000/").then(res => res.json());
 
-export const getTodosid = (id) => fetch(`http://localhost:4000/${id}`).then(res => res.json());
+let url="https://mern-rest-api-test.herokuapp.com"
 
-export const deleteTodo = (id) => fetch(`http://localhost:4000/delete/${id}`, {
+export const getTodos = () => fetch(url).then(res => res.json());
+
+export const getTodosid = (id) => fetch(`${url}/${id}`).then(res => res.json());
+
+export const deleteTodo = (id) => fetch(`${url}/delete/${id}`, {
   method: "POST",
   mode: 'no-cors',
   headers: {
@@ -24,7 +27,7 @@ export const deleteTodo = (id) => fetch(`http://localhost:4000/delete/${id}`, {
 export const createTodo = (todo) => {
   // console.log(todo);
 
-   fetch(`http://localhost:4000/${todo}`, {
+   fetch(`${url}/${todo}`, {
   method: 'POST',
   mode: 'no-cors',
   headers: {
@@ -62,7 +65,7 @@ export const createTodo = (todo) => {
 export const patchTodo = (todo,id) => 
   // console.log(todo);
 
-   fetch(`http://localhost:4000/update/${id}/${todo}`, {
+   fetch(`${url}/update/${id}/${todo}`, {
   method: 'POST',
   mode: 'no-cors',
   headers: {
